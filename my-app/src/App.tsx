d825bd5dfb2b518e0fa38a9176b94dfc4b625134
Sungho <hurgj123kr@gmail.com>
2024-05-03 20:28:03 +0900

    generics에 대해 counte로 공부

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,12 +1,9 @@
 import React from "react";
 import "./App.css";
-import Greetings from "./Greetings";
+import Counter from "./Counter";
 
 const App: React.FC = () => {
-  const onClick = (name: string) => {
-    console.log(`${name} says hello`);
-  };
-  return <Greetings name="React" onClick={onClick} />;
+  return <Counter />;
 };
 
 export default App;
---
+++ my-app/src/Counter.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  const onIncrease = () => setCount(count + 1);
+  const onDecrease = () => setCount(count - 1);
+  return (
+    <div>
+      <h1>{count}</h1>
+      <div>
+        <button onClick={onIncrease}>Up</button>
+        <button onClick={onDecrease}>Down</button>
+      </div>
+    </div>
+  );
+}
+export default Counter;
