bd5f6efd023f271cc84eba4c776657a3ae94dabe
Sungho <hurgj123kr@gmail.com>
2024-05-07 20:32:57 +0900

    modules todo add

--- my-app/src/modules/index.ts
+++ my-app/src/modules/index.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import counter from "./counter";
+import todos from "./todos";
 
 const rootReducer = combineReducers({
   counter,
+  todos,
 });
 
 // 루트 리듀서 내보내기
