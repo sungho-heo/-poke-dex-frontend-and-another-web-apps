92de9901073008627728d9f11d947a6808097338
Sungho <hurgj123kr@gmail.com>
2024-05-14 20:58:02 +0900

    github lib reducerUtils create

---
+++ my-app/src/lib/reducerUtils.ts
@@ -0,0 +1,32 @@
+import { AnyAction } from "redux";
+import { getType } from "typesafe-actions";
+
+export type AsyncState<T, E = any> = {
+  data: T | null;
+  loading: boolean;
+  error: E | null;
+};
+
+export const asyncState = {
+  // 화살표 함수에 Generic을 설정함.
+  initial: <T, E = any>(initialData?: T): AsyncState<T, E> => ({
+    loading: false,
+    data: initialData || null,
+    error: null,
+  }),
+  load: <T, E = any>(data?: T): AsyncState<T, E> => ({
+    loading: true,
+    data: data || null,
+    error: null,
+  }),
+  success: <T, E = any>(data: T): AsyncState<T, E> => ({
+    loading: false,
+    data,
+    error: null,
+  }),
+  error: <T, E>(error: E): AsyncState<T, E> => ({
+    loading: false,
+    data: null,
+    error: error,
+  }),
+};
