96785700ec972c4823ec781f4c513a19d468323a
Sungho <hurgj123kr@gmail.com>
2024-05-25 20:56:00 +0900

    pokemon search create

--- pokemon-dex/src/components/PokemonList.tsx
+++ pokemon-dex/src/components/PokemonList.tsx
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQuery, useQueries, UseQueryResult } from "@tanstack/react-query";
 import { fetchPokemonList, fetchPokemon, PokemonData } from "../api";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
-import { Container } from "../styles/CommonStyles";
+import { Container, GridContainer } from "../styles/CommonStyles";
 
 // Css 세팅
+
+const SearchInput = styled.input`
+  padding: 10px;
+  margin: 20px 0;
+  width: 100%;
+  max-width: 400px;
+  font-size: 16px;
+`;
+
 const PokemonCard = styled.div`
   border: 2.5px solid #ccc;
   border-radius: 10px;
   padding: 20px;
   margin: 10px;
-  display: inline-block;
   width: 200px;
   text-align: center;
 `;
@@ -24,6 +32,8 @@ const PokemonImage = styled.img`
 
 // 포켓몬 데이터 가져오기 20번까지
 const PokemonList: React.FC = () => {
+  const [searchPokemon, setSearchPokemon] = useState<string>("");
+
   const {
     data: listData,
     error: listError,
@@ -46,34 +56,56 @@ const PokemonList: React.FC = () => {
     // UseQueryResult가 해당 역할을 해줌.
   }) as UseQueryResult<PokemonData>[];
 
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchPokemon(event.target.value);
+  };
+
+  const filterPokemonQueries = searchPokemon
+    ? pokemonQueries.filter((query) =>
+        query.data?.name.toLowerCase().includes(searchPokemon.toLowerCase())
+      )
+    : pokemonQueries;
+
   if (listLoading) return <Container>...Loading</Container>;
   if (listError instanceof Error)
     return <Container>Error: {listError.message}</Container>;
 
   return (
     <Container>
-      {pokemonQueries.map((query, index) => {
-        const { data, error, isLoading } = query;
+      <SearchInput
+        type="text"
+        placeholder="포켓몬 이름을 검색하세요."
+        value={searchPokemon}
+        onChange={handleSearch}
+      />
+      <GridContainer>
+        {filterPokemonQueries.map((query, index) => {
+          const { data, error, isLoading } = query;
 
-        if (isLoading) return <PokemonCard key={index}>Loading...</PokemonCard>;
-        if (error instanceof Error)
-          return <PokemonCard key={index}>Error: {error.message}</PokemonCard>;
-        return (
-          // data?해당 뜻은 포켓몬 api로부터 데이터를 못가져올경우 undifinded로 가져오게하기위해서 즉 에리가 발생하기위해서임.
-          <PokemonCard key={data?.name}>
-            <h2>{data?.name}</h2>
-            <Link to={`/pokemon/${data?.name}`}>
-              <PokemonImage
-                src={data?.sprites.front_default}
-                alt={data?.name}
-              />
-            </Link>
-            <p>
-              Type:{data?.types.map((typeInfo) => typeInfo.type.name).join(",")}
-            </p>
-          </PokemonCard>
-        );
-      })}
+          if (isLoading)
+            return <PokemonCard key={index}>Loading...</PokemonCard>;
+          if (error instanceof Error)
+            return (
+              <PokemonCard key={index}>Error: {error.message}</PokemonCard>
+            );
+          return (
+            // data?해당 뜻은 포켓몬 api로부터 데이터를 못가져올경우 undifinded로 가져오게하기위해서 즉 에리가 발생하기위해서임.
+            <PokemonCard key={data?.name}>
+              <h2>{data?.name}</h2>
+              <Link to={`/pokemon/${data?.name}`}>
+                <PokemonImage
+                  src={data?.sprites.front_default}
+                  alt={data?.name}
+                />
+              </Link>
+              <p>
+                Type:
+                {data?.types.map((typeInfo) => typeInfo.type.name).join(",")}
+              </p>
+            </PokemonCard>
+          );
+        })}
+      </GridContainer>
     </Container>
   );
 };
--- pokemon-dex/src/styles/CommonStyles.ts
+++ pokemon-dex/src/styles/CommonStyles.ts
@@ -4,4 +4,16 @@ import styled from "styled-components";
 export const Container = styled.div`
   padding: 20px;
   text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const GridContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  gap: 10px;
+  justify-content: center;
+  width: 100%;
+  max-width: 1200px;
 `;
