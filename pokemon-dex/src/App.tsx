7e8a102f87614dbc682255034a2e588f0a5366f8
Sungho <hurgj123kr@gmail.com>
2024-06-02 21:06:17 +0900

    login button and signup button create and profile page

--- pokemon-dex/src/App.tsx
+++ pokemon-dex/src/App.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import queryClient from "./queryClient";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
 import PokemonList from "./components/PokemonList";
 import PokemonDetail from "./components/PokemonDetail";
+import Profile from "./components/Profile";
 import "./App.css";
 
 const App: React.FC = () => {
@@ -12,10 +15,21 @@ const App: React.FC = () => {
       {/* 라우터 설정. */}
       <Router>
         <div className="App">
-          <h1>Pokemon Dex</h1>
+          <header>
+            <h1>Pokemon Dex</h1>
+            <nav>
+              <Link to="/">home</Link>
+              <Link to="/login">Login</Link>
+              <Link to="/signup">Signup</Link>
+              <Link to="/profile">Profile</Link>
+            </nav>
+          </header>
           <Routes>
             <Route path="/" element={<PokemonList />} />
             <Route path="/pokemon/:name" element={<PokemonDetail />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
           </Routes>
         </div>
       </Router>
---
+++ pokemon-dex/src/components/Profile.tsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import { useAuth } from "../context/AuthContext";
+
+// user Profile
+const Profile: React.FC = () => {
+  const token = useAuth();
+  const [fav, setFav] = useState<string[]>([]);
+
+  useEffect(() => {
+    const fetchFav = async () => {
+      if (token) {
+        const response = await fetch("http://localhost:5000/api/fav", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        const data = await response.json();
+        setFav(data.fav);
+      }
+    };
+    fetchFav();
+  }, [token]);
+  if (!token) {
+    return <p>Plz Signup</p>;
+  }
+  return (
+    <div>
+      <h2>Your Poketmon</h2>
+      {fav.length > 0 ? (
+        <ul>
+          {fav.map((name) => (
+            <li key={name}>{name}</li>
+          ))}
+        </ul>
+      ) : (
+        <p>You no Poketmon.</p>
+      )}
+    </div>
+  );
+};
+
+export default Profile;
